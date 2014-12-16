a42a3453f74853b3bc22744252d64d92078bdf8d
Emilio <Emilio@epc>
2014-12-16 12:32:57 +0100

    arreglo

--- web/js/maps.js
+++ web/js/maps.js
@@ -28,7 +28,7 @@ function cargarMapa(idPost,gps,contentStr){
     var lat = gpsSplit[0];
     var long = gpsSplit[1];
     $("#mapa"+idPost.toString()).click(function(){
-        $("#mostrarMapa"+idPost.toString()).load("/RocksteadyRep/jsp/maps.jsp?latitud=44&longitud=-2");
+        $("#mostrarMapa"+idPost.toString()).load("/RocksteadyRep/maps.jsp?latitud=44&longitud=-2");
     });
 }
 
